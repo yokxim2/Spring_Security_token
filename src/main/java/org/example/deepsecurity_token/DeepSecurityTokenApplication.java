541b6b3a2ffa98f2fe2dbfc3dd4b4a116a67359c
yokxim <yooksi53@gmail.com>
2025-02-01 15:47:20 +0900

    Initial commit

---
+++ src/main/java/org/example/deepsecurity_token/DeepSecurityTokenApplication.java
@@ -0,0 +1,13 @@
+package org.example.deepsecurity_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepSecurityTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepSecurityTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/deepsecurity_token/DeepSecurityTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.deepsecurity_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepSecurityTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

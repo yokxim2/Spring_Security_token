27f4f6367715523e2385e4bc96164d9bf608232a
yokxim <yooksi53@gmail.com>
2025-02-01 21:14:23 +0900

    fix[security-config] CORS 에러를 막기 위해서 백엔드에서 설정 해 주어야 하는 것들이 있다.
    - 3000번 포트로부터 오는 요청들에 있어서
    - 모든 메서드, 헤더, credentials, 열어놓기

--- src/main/java/org/example/deepsecurity_token/config/SecurityConfig.java
+++ src/main/java/org/example/deepsecurity_token/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.example.deepsecurity_token.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.deepsecurity_token.jwt.JWTFilter;
 import org.example.deepsecurity_token.jwt.JWTUtil;
 import org.example.deepsecurity_token.jwt.LoginFilter;
@@ -13,6 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -43,6 +48,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowCredentials(true);
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L);
+
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        }));
 
         //csrf disable
         http

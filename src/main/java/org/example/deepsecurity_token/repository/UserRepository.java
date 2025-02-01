7cf00fc01bd19aec4aa571b6fdafcf596c87afcf
yokxim <yooksi53@gmail.com>
2025-02-01 16:41:57 +0900

    fix[user-repository] 중복확인 확인용 메서드 추가

--- src/main/java/org/example/deepsecurity_token/repository/UserRepository.java
+++ src/main/java/org/example/deepsecurity_token/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.example.deepsecurity_token.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Boolean existsByUsername(String username);
 }

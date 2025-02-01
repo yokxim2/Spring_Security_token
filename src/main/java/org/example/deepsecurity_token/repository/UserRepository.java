1f8fe3960a2fc8c377b0e1790af1eaa9bae0c9df
yokxim <yooksi53@gmail.com>
2025-02-01 16:25:14 +0900

    feat[user-repository] 유저 데이터 저장소 클래스 추가

---
+++ src/main/java/org/example/deepsecurity_token/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package org.example.deepsecurity_token.repository;
+
+import org.example.deepsecurity_token.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}

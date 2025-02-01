d98dd5d05b045850061ddb836735dd162b6aaa1a
yokxim <yooksi53@gmail.com>
2025-02-01 21:11:30 +0900

    fix[main-controller] MainController 쪽에서도 SecurityContextHolder를 사용해서 UserDetails 값을 빼올 수 있다.

--- src/main/java/org/example/deepsecurity_token/controller/MainController.java
+++ src/main/java/org/example/deepsecurity_token/controller/MainController.java
@@ -1,8 +1,14 @@
 package org.example.deepsecurity_token.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -11,6 +17,14 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "<Main Controller> Hi " + username + " " + role;
     }
 }

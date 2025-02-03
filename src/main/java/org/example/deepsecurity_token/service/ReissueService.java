1a0aad4fa364817c36c83dab21f6d77b0eb22c84
yokxim <yooksi53@gmail.com>
2025-02-03 22:39:21 +0900

    fix[reissue-service] Refresh Token 재발급 시 DB에 저장되어 있던 값 갱신

--- src/main/java/org/example/deepsecurity_token/service/ReissueService.java
+++ src/main/java/org/example/deepsecurity_token/service/ReissueService.java
@@ -2,18 +2,24 @@ package org.example.deepsecurity_token.service;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
+import org.example.deepsecurity_token.entity.RefreshEntity;
 import org.example.deepsecurity_token.exception.CustomJwtException;
 import org.example.deepsecurity_token.jwt.JWTUtil;
+import org.example.deepsecurity_token.repository.RefreshRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class ReissueService {
 
     private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
-    public ReissueService(JWTUtil jwtUtil) {
+    public ReissueService(JWTUtil jwtUtil, RefreshRepository refreshRepository) {
         this.jwtUtil = jwtUtil;
+        this.refreshRepository = refreshRepository;
     }
 
     public String[] reissue(Cookie[] cookies) {
@@ -54,6 +60,12 @@ public class ReissueService {
             throw new CustomJwtException("Refresh token is invalid", HttpStatus.BAD_REQUEST);
         }
 
+        // DB에 저장이 되어 있는지 확인
+        Boolean isExist = refreshRepository.existsByRefresh(refresh);
+        if (!isExist) {
+            throw new CustomJwtException("Refresh token is invalid", HttpStatus.BAD_REQUEST);
+        }
+
         String username = jwtUtil.getUsername(refresh);
         String role = jwtUtil.getRole(refresh);
 
@@ -61,9 +73,25 @@ public class ReissueService {
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
         String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
+        // Refresh 토큰 저장 DB에 기존의 Refresh 토큰 삭제 후 새 Refresh 토큰 저장
+        refreshRepository.deleteByRefresh(refresh);
+        addRefreshEntity(username, newRefresh, 86400000L);
+
         tokens[0] = newAccess;
         tokens[1] = newRefresh;
 
         return tokens;
+    }
+
+    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
+
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+
+        RefreshEntity refreshEntity = new RefreshEntity();
+        refreshEntity.setUsername(username);
+        refreshEntity.setRefresh(refresh);
+        refreshEntity.setExpiration(date.toString());
+
+        refreshRepository.save(refreshEntity);
     }
 }

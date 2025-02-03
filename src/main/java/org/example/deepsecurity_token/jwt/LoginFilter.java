b36f062e329d7b7f4a7256abf02527c720819a2a
yokxim <yooksi53@gmail.com>
2025-02-03 22:37:02 +0900

    fix[login-filter] 인증 성공 시 인증에 사용된 Refresh 토큰을 저장하는 로직 추가

--- src/main/java/org/example/deepsecurity_token/jwt/LoginFilter.java
+++ src/main/java/org/example/deepsecurity_token/jwt/LoginFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.deepsecurity_token.dto.CustomUserDetails;
+import org.example.deepsecurity_token.entity.RefreshEntity;
+import org.example.deepsecurity_token.repository.RefreshRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,16 +16,18 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
+    private final RefreshRepository refreshRepository;
     private final JWTUtil jwtUtil;
 
-    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
-
+    public LoginFilter(AuthenticationManager authenticationManager, RefreshRepository refreshRepository, JWTUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
+        this.refreshRepository = refreshRepository;
         this.jwtUtil = jwtUtil;
     }
 
@@ -56,6 +60,9 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String access = jwtUtil.createJwt("access", username, role, 600000L);
         String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
+        //Refresh 토큰 저장
+        addRefreshEntity(username, refresh, 86400000L);
+
         //응답 설정
         response.setHeader("access", access);
         response.addCookie(createCookie("refresh", refresh));
@@ -86,6 +93,18 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         cookie.setHttpOnly(true);
 
         return cookie;
+    }
+
+    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
+
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+
+        RefreshEntity refreshEntity = new RefreshEntity();
+        refreshEntity.setUsername(username);
+        refreshEntity.setRefresh(refresh);
+        refreshEntity.setExpiration(date.toString());
+
+        refreshRepository.save(refreshEntity);
     }
 
     //로그인 실패시 실행하는 메소드
